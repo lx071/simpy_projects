25a28bb6d0e262abdc1b8649df10473567c81042
lx071 <onlyfor_ever@163.com>
2024-06-13 08:01:17 -0700

    update

--- simpy-pybind/README.md
+++ simpy-pybind/README.md
@@ -2,7 +2,9 @@ example_v0: SimPy + Cpp（via pybind11）
 example_v1: SimPy + Cpp + Verilog（via pybind11 + verilator）(include Cpp + Verilog)  
 example_v2: use Python to generate Cpp  
 example_v3: use SimPy to implement TLM_v0  
-example_v4: use SimPy to implement TLM_v1  
-example_v5: implement sequencer + driver + dut  
+example_v4: use SimPy to implement TLM_v1（Generic_Payload + Module + Socket）
+example_v5: implement Sequencer + Driver + DUT  
+example_v6: implement Sequence_Item + Sequence + Sequencer + Driver + DUT  
+example_v6: implement class uvm_sequence_item + uvm_tlm_generic_payload + uvm_sequence + uvm_sequencer + uvm_driver  
 alu_example: SimPy + Cpp + Verilog（via pybind11 + verilator）  
 
--- simpy-pybind/example_v7/tinyalu.py
+++ simpy-pybind/example_v7/tinyalu.py
@@ -1,5 +1,4 @@
-from utils.tlm_utils import *
-from utils.harness_utils import sim
+from utils import *
 import simpy
 import random
 
@@ -17,18 +16,11 @@ import random
 config_db = {}
 
 
-class Sequence(Sequence_Item):
+class Sequence(uvm_sequence):
 
     def __init__(self, env, name):
-        super().__init__(name)
-        self.env = env
+        super().__init__(env, name)
 
-
-    def start(self, sequencer, parent_sequence = None):
-        self.m_sequencer = sequencer
-        self.set_item_context(parent_sequence, sequencer)
-        self.env.process(self.body())
-        
 
     def body(self):
         # in1 = [random.randrange(0, 20) for i in range(20)]
@@ -51,145 +43,21 @@ class Sequence(Sequence_Item):
         }
         item.set_data_ptr(payload)
         yield self.env.process(self.finish_item(item))
+
+
+class Sequencer(uvm_sequencer):
     
-
-    def create_item(self):
-        trans = Generic_Payload()
-        return trans
-    
-
-    def start_item(self, item, sequencer):
-        item.set_item_context(self, sequencer)
-        # self.m_sequencer.wait_for_grant(this, set_priority)
-        '''a user-definable callback task that is called ~on the parent sequence~ after the sequencer has selected this sequence, and before the item is randomized.''' 
-        # pre_do(1) 
-        pass
-
-
-    def finish_item(self, item):
-        sequencer = item.get_sequencer()
-        if sequencer == None:
-            # uvm_report_fatal("STRITM", "sequence_item has null sequencer", UVM_NONE);
-            print("FATAL:", "sequence_item has null sequencer")
-            raise ResponseError("sequence_item has null sequencer")
-        
-        '''a user-definable callback function that is called after the sequence item has been randomized, and just before the item is sent to the driver.'''        
-        # mid_do(item); 
-
-        yield self.env.process(sequencer.send_request(self, item))
-        yield self.env.process(sequencer.wait_for_item_done(self, -1))
-        '''a user-definable callback function that is called after the driver has indicated that it has completed the item, using either this item_done or put methods.'''
-        # post_do(item);
-
-
-    def send_request(self, request, rerandomize = 0):
-        if self.m_sequencer == None:
-            # uvm_report_fatal("SSENDREQ", "Null m_sequencer reference", UVM_NONE);
-            print("FATAL:", "Null m_sequencer reference")
-            raise ResponseError("Null m_sequencer reference")
-        self.m_sequencer.send_request(self, request, rerandomize)
-
-
-    def wait_for_item_done(self, transaction_id = -1):
-        if self.m_sequencer == None:
-            # uvm_report_fatal("WAITITEMDONE", "Null m_sequencer reference", UVM_NONE);
-            print("FATAL:", "Null m_sequencer reference")
-            raise ResponseError("Null m_sequencer reference")
-        self.m_sequencer.wait_for_item_done(self, transaction_id)
-
-
-class Sequencer(Module):
-    
-    m_id = 0
-
     def __init__(self, env, name):
         super().__init__(env, name)
-        self.sequence_item_requested = 0
-        self.get_next_item_called = 0
-
-        self.socket = Socket(self)
-
-        self.socket.register_get_next_item(self.get_next_item)
-        self.socket.register_item_done(self.item_done)
-
-        # uvm_tlm_fifo #(REQ) m_req_fifo;
-        self.store = simpy.Store(env, capacity=1)
-
-        self.reg_sequences = []
-
-        Sequencer.m_id += 1
-        self.m_sequencer_id = Sequencer.m_id
-
-        self.m_wait_for_item_sequence_id = 0
-        self.m_wait_for_item_transaction_id = 0
-
-        self.item_done_e = self.env.event()
 
 
-    def get_next_item(self, trans):
-        ptr = trans.get_data_ptr()
-
-        if self.get_next_item_called == 1:
-            # uvm_report_error(get_full_name(), "Get_next_item called twice without item_done or get in between", UVM_NONE);
-            print("ERROR:", "Get_next_item called twice without item_done or get in between")
-        
-        # if not self.sequence_item_requested:
-        #     m_select_sequence()
-
-        # Set flag indicating that the item has been requested to ensure that item_done or get
-        # is called between requests
-        self.sequence_item_requested = 1
-        self.get_next_item_called = 1
-        # self.m_req_fifo.peek(t)
-        
-        item = yield self.store.get()
-        
-        ptr[0] = item.get_data_ptr()
-        trans.set_response_status(tlm_response_status.TLM_OK_RESPONSE, self.socket.other_socket)
-    
-
-    def item_done(self, item = None):
-        self.sequence_item_requested = 0
-        self.get_next_item_called = 0
-
-        self.item_done_e.succeed()
-        self.item_done_e = self.env.event()  
-        if item != None:
-            self.put_response(item)
-
-
-    def send_request(self, sequence_ptr, t: Generic_Payload, rerandomize = 0):
-        t.set_sequencer(self)
-        # if self.m_req_fifo.try_put(t) != 1:
-        yield self.store.put(t)
-        #     # uvm_report_fatal(get_full_name(), "Concurrent calls to get_next_item() not supported. Consider using a semaphore to ensure that concurrent processes take turns in the driver", UVM_NONE);
-        #     print("Concurrent calls to get_next_item() not supported. Consider using a semaphore to ensure that concurrent processes take turns in the driver")
-        #     raise ResponseError("Concurrent calls to get_next_item() not supported. Consider using a semaphore to ensure that concurrent processes take turns in the driver")
-
-
-    def put_response(self, t: Sequence_Item):
-        sequence_ptr = self.reg_sequences[t.get_sequence_id()]
-        sequence_ptr.put_response(t)
-
-
-    def wait_for_item_done(self, sequence_ptr, transaction_id):
-        # sequence_id = sequence_ptr.m_get_sqr_sequence_id(self.m_sequencer_id, 1)
-        # self.m_wait_for_item_sequence_id = -1
-        # self.m_wait_for_item_transaction_id = -1
-        yield self.item_done_e
-        pass
-
-
-class Driver(Module):
+class Driver(uvm_driver):
 
     def __init__(self, env, name):
         super().__init__(env, name)
 
         self.simContext = config_db['simContext']
 
-        # 创建 socket，与 sequencer 进行连接
-        self.socket = Socket(self)
-        
         self.run_thread = self.env.process(self.run())
         self.bfm_thread = self.env.process(self.bfm())
 
@@ -206,7 +74,7 @@ class Driver(Module):
 
 
     def run(self):
-        trans = Generic_Payload()
+        trans = uvm_tlm_generic_payload()
         trans.set_data_ptr( self.data )
 
         while True:
@@ -330,8 +198,8 @@ def test_tinyalu():
     # 创建顶层模块 top，传入 env
     top = Top(env, 'top')
 
-    sequence = Sequence(env, 'seq')
-    sequence.start(top.sequencer)
+    seq = Sequence(env, 'seq')
+    seq.start(top.sequencer)
 
     # 运行仿真
     env.run()
