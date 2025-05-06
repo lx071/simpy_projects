e91934f43327a59a47ae43ff30165b5ca40cb656
lx071 <onlyfor_ever@163.com>
2025-05-06 02:39:18 -0700

    update

--- simpy_pybind/tinyalu_test/sysc_test/tlm1_example/tlm1.cpp
+++ simpy_pybind/tinyalu_test/sysc_test/tlm1_example/tlm1.cpp
@@ -3,80 +3,38 @@
 
 #include "utils.h"
 
-// Sequencer构造函数实现
-uvm_sequencer::uvm_sequencer(sc_core::sc_module_name name) 
-    : sc_module(name), intf(*this) 
-{
-    seq_item_export.bind(intf);
-}
-
-// Sequencer方法实现
-void uvm_sequencer::get_next_item(tlm::tlm_generic_payload& trans, 
-                            sc_core::sc_time& delay) 
-{
-    std::cout << "Getting next item at " 
-              << sc_core::sc_time_stamp() 
-              << std::endl;
-}
-
-void uvm_sequencer::item_done(tlm::tlm_generic_payload& trans,
-                        sc_core::sc_time& delay) 
-{
-    std::cout << "Item done at " 
-              << sc_core::sc_time_stamp() 
-              << std::endl;
-}
-
-
-// 适配器方法实现
-uvm_seq_item_if::uvm_seq_item_if(uvm_sequencer& comp) 
-    : imp(comp) {}
-
-void uvm_seq_item_if::get_next_item(tlm::tlm_generic_payload& trans,
-                                           sc_core::sc_time& delay) 
-{
-    imp.get_next_item(trans, delay);
-}
-
-void uvm_seq_item_if::item_done(tlm::tlm_generic_payload& trans,
-                                       sc_core::sc_time& delay) 
-{
-    imp.item_done(trans, delay);
-}
-
-
-uvm_driver::uvm_driver(sc_core::sc_module_name name) 
-    : sc_module(name) 
-{
-    // 注册测试线程
-    SC_THREAD(run_test);
-}
-
-void uvm_driver::run_test() {
-    // 创建测试事务
-    tlm::tlm_generic_payload trans;
-    sc_core::sc_time delay(10, SC_NS);
-    
-    // 测试场景
-    for (int i = 0; i < 3; ++i) {
-        // 获取下一个事务
-        seq_item_port->get_next_item(trans, delay);
-        
-        // 模拟处理延迟
-        wait(25, SC_NS);
-        
-        // 标记事务完成
-        seq_item_port->item_done(trans, delay);
-        
-        // 间隔
-        wait(50, SC_NS);
+class driver: public uvm_driver {
+public:
+    driver(sc_core::sc_module_name name) 
+    : uvm_driver(name) 
+    {
     }
-}
+    void run_phase() override {
+        // 创建测试事务
+        tlm::tlm_generic_payload trans;
+        sc_core::sc_time delay(10, SC_NS);
+        
+        // 测试场景
+        for (int i = 0; i < 3; ++i) {
+            // 获取下一个事务
+            seq_item_port->get_next_item(trans, delay);
+            
+            // 模拟处理延迟
+            wait(25, SC_NS);
+            
+            // 标记事务完成
+            seq_item_port->item_done(trans, delay);
+            
+            // 间隔
+            wait(50, SC_NS);
+        }
+    }
+};
 
 int sc_main(int argc, char* argv[]) {
     // 创建模块实例
     uvm_sequencer sequencer("Sequencer");
-    uvm_driver driver("Driver");
+    driver driver("Driver");
     
     // 连接端口
     driver.seq_item_port(sequencer.seq_item_export);
--- simpy_pybind/tinyalu_test/sysc_test/tlm1_example/utils.h
+++ simpy_pybind/tinyalu_test/sysc_test/tlm1_example/utils.h
@@ -1,9 +1,9 @@
-#ifndef SEQUENCER_H
-#define SEQUENCER_H
+#ifndef UTILS_H
+#define UTILS_H
 
 #include <systemc.h>
 #include <tlm.h>
-
+#include <queue>
 
 // 前向声明
 class uvm_sequencer;
@@ -24,16 +24,34 @@ public:
     sc_core::sc_export<uvm_seq_item_if> seq_item_export;
     uvm_seq_item_if intf;
 
-    SC_CTOR(uvm_sequencer);
-    // uvm_driver(sc_module_name name) : sc_module(name) {
-    //     SC_THREAD(run_phase);
-    // }
-     
-    void get_next_item(tlm::tlm_generic_payload& trans,
-                     sc_core::sc_time& delay);
+    // Sequencer构造函数实现
+    uvm_sequencer(sc_core::sc_module_name name) 
+        : sc_module(name), intf(*this) 
+    {
+        seq_item_export.bind(intf);
+    }
+
+    // Sequencer方法实现
+    void get_next_item(tlm::tlm_generic_payload& trans, 
+                                sc_core::sc_time& delay) 
+    {
+        std::cout << "Getting next item at " 
+                << sc_core::sc_time_stamp() 
+                << std::endl;
+    }
+
     void item_done(tlm::tlm_generic_payload& trans,
-                 sc_core::sc_time& delay);
+                            sc_core::sc_time& delay) 
+    {
+        std::cout << "Item done at " 
+                << sc_core::sc_time_stamp() 
+                << std::endl;
+    }
+
+    std::queue<tlm::tlm_generic_payload*> m_req_fifo;
 };
+
+
 
 class uvm_driver : public sc_module {
 public:
@@ -41,14 +59,36 @@ public:
     sc_core::sc_port<uvm_seq_item_if> seq_item_port;
     
     // 构造函数
-    SC_CTOR(uvm_driver);
-    // uvm_driver(sc_module_name name) : sc_module(name) {
-    //     SC_THREAD(run_phase);
-    // }
-    
-private:
+    uvm_driver(sc_core::sc_module_name name) 
+    : sc_module(name) 
+    {
+        SC_HAS_PROCESS(uvm_driver);
+        // 注册测试线程
+        SC_THREAD(run_phase);
+    }
+
     // 测试线程
-    void run_test();
+    virtual void run_phase() 
+    {
+        std::cout << "uvm_driver::run_phase" << std::endl;
+    };
 };
 
-#endif // SEQUENCER_H
+
+// interface 方法实现
+uvm_seq_item_if::uvm_seq_item_if(uvm_sequencer& comp) 
+    : imp(comp) {}
+
+void uvm_seq_item_if::get_next_item(tlm::tlm_generic_payload& trans,
+                                           sc_core::sc_time& delay) 
+{
+    imp.get_next_item(trans, delay);
+}
+
+void uvm_seq_item_if::item_done(tlm::tlm_generic_payload& trans,
+                                       sc_core::sc_time& delay) 
+{
+    imp.item_done(trans, delay);
+}
+
+#endif // UTILS_H
